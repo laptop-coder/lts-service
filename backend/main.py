636cf5df9dc7e720cd444d31fc146a30e78dea6a
laptop-coder <coderlaptop@gmail.com>
2024-11-19 21:48:17 +0300

    add a router template

--- backend/main.py
+++ backend/main.py
@@ -2,3 +2,25 @@ from fastapi import FastAPI
 
 
 app = FastAPI()
+
+
+@app.get("/get_things_list")
+def get_things_list(type: str):
+    if type == "lost":
+        pass
+    elif type == "found":
+        pass
+    
+
+@app.post("/add_new_thing")
+def add_new_thing(type: str):
+    if type == "lost":
+        pass
+    elif type == "found":
+        pass
+
+
+@app.get("/change_thing_status")
+def change_thing_status(id: int):
+    pass
+    
