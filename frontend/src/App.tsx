95b6f7f620b58268c96a70c9910572994c7ce0df
laptop-coder <coderlaptop@gmail.com>
2024-11-23 22:24:51 +0300

    divide the page into main blocks

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,7 +5,25 @@ import styles from './app.module.css';
 
 const App: Component = () => {
   return (
-    <div>
+    <div class={styles.page}>
+      <div class={styles.header}>
+        <div class={styles.header__title}>
+	</div>
+        <div class={styles.header__buttons}>
+	</div>
+      </div>
+      <div class={styles.content}>
+        <div class={styles.things_list}>
+	  <div class={styles.things_list__title}>
+	  </div>
+	</div>
+        <div class={styles.things_list}>
+	  <div class={styles.things_list__title}>
+	  </div>
+	</div>
+      </div>
+      <div class={styles.footer}>
+      </div>
     </div>
   );
 };
