7135420c1f96bae066a2a220573b695c154c23f5
laptop-coder <coderlaptop@gmail.com>
2025-01-01 18:17:11 +0300

    fix setup script

--- setup.sh
+++ setup.sh
@@ -5,10 +5,10 @@ cd ./frontend
 npm i
 cd ../backend
 mkdir -p ./storage/{lost,found}
-cat ./db.sql | sqlite3 ./backend/db.sqlite3
+cat ./db.sql | sqlite3 ./db.sqlite3
 python3 -m venv env
 . ./env/bin/activate
 pip install -r ./requirements.txt
-clean
+clear
 cd ../
 
