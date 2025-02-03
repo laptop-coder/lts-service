32bd97a566d93b566daada37042ec9810d208c6f
laptop-coder <coderlaptop@gmail.com>
2025-02-03 08:53:29 +0300

    refactor

--- frontend/src/app/index.tsx
+++ frontend/src/app/index.tsx
@@ -4,10 +4,4 @@ import { HomePage } from "../pages/home/index";
 
 const root = document.getElementById("root");
 
-if (import.meta.env.DEV && !(root instanceof HTMLElement)) {
-  throw new Error(
-    "Root element not found. Did you forget to add it to your index.html? Or maybe the id attribute got misspelled?",
-  );
-}
-
 render(() => <HomePage />, root!);
