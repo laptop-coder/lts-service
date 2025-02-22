2002a78cf69a4f66c89e20464e3b1d3d86be0e0b
laptop-coder <coderlaptop@gmail.com>
2025-02-22 13:48:36 +0300

    scripts (`setup.sh` and `run_dev.sh`)
    - Improve the output of the `setup.sh` script
    - Change `run_dev.sh` script

--- run_dev.sh
+++ run_dev.sh
@@ -2,8 +2,7 @@
 # Script to run this project in development mode.
 # Run this script from the project's root directory (where this script is located).
 cd ./frontend
-alacritty -e npm run dev &
-disown
+npm run dev &
 cd ../backend
 . ./env/bin/activate
 fastapi dev main.py
--- setup.sh
+++ setup.sh
@@ -1,14 +1,51 @@
 #!/bin/sh
 # Script to setup this project for development.
 # Run this script from the project's root directory (where this script is located)
+
+function info {
+  printf "\033[1m$1\033[0m\n"
+}
+
+function result {
+  if [ $? -eq 0 ]; then
+    printf "\033[1;32mDone\033[0m\n"
+  else
+    printf "\033[1;31mError\033[0m\n"
+  fi
+}
+
 cd ./frontend
-npm i
+
+info "Installing Npm requirements..."
+npm i > /dev/null
+result
+
 cd ../backend
-mkdir -p ./storage/{lost,found}
-cat ./db.sql | sqlite3 ./db.sqlite3
-python3 -m venv env
-. ./env/bin/activate
-pip install -r ./requirements.txt
-clear
+
+info "Creating storage..."
+mkdir -p ./storage/{lost,found} > /dev/null
+result
+
+info "Creating database..."
+cat ./db.sql | sqlite3 ./db.sqlite3 > /dev/null
+result
+
+info "Creating Python virtual environment..."
+python3 -m venv env > /dev/null
+result
+
+info "Activating Python virtual environment..."
+. ./env/bin/activate > /dev/null
+result
+
+info "Updating Python package manager..."
+pip install --upgrade pip > /dev/null
+result
+
+info "Installing Python requirements..."
+pip install -r ./requirements.txt > /dev/null
+result
+
 cd ../
+
 
