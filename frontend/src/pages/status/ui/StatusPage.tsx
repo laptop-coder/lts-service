73434d33720302948dcf18e1034f6ad48da75774
laptop-coder <coderlaptop@gmail.com>
2025-05-20 23:11:07 +0300

    feat(frontend): add header to the status page

--- frontend/src/pages/status/ui/StatusPage.tsx
+++ frontend/src/pages/status/ui/StatusPage.tsx
@@ -5,5 +5,22 @@ import "../../../app/styles.css";
 export const StatusPage: Component = () => {
   const params = useParams();
   console.log(params.type, params.id);
-  return <div class="page"></div>;
+  return (
+    <div class="page">
+      <div class="header">
+        <a
+          class="header__wrapper"
+          href="/home"
+          title="На главную"
+        >
+          <img
+            class="header__logo"
+            src="/logo.svg"
+            style="cursor: pointer"
+          />
+          <div class="header__title">Сервис поиска потерянных вещей</div>
+        </a>
+      </div>
+    </div>
+  );
 };
