901ae3f95c2990dff7083998797d9315e5f2e47a
laptop-coder <coderlaptop@gmail.com>
2025-06-19 13:16:33 +0300

    refactor(frontend): promises
    `resolve` and `reject` in promises

--- frontend/src/entities/lostThing/api/changeThingStatus.ts
+++ frontend/src/entities/lostThing/api/changeThingStatus.ts
@@ -2,7 +2,8 @@ import { GET } from '../../../shared/lib/utils/index';
 
 export const changeThingStatus = (id: number) => {
   return new Promise(async (resolve, reject) => {
-    await GET(`change_thing_status?type=lost&id=${id}`);
-    resolve();
+    await GET(`change_thing_status?type=lost&id=${id}`)
+      .then((response) => resolve(response))
+      .catch((error) => reject(error));
   });
 };
