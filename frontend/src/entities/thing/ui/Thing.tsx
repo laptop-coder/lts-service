72ab6b5dd40090692f96009406ab74022e0c3e4d
laptop-coder <coderlaptop@gmail.com>
2025-06-20 21:51:30 +0300

    refactor(frontend): fix `props`
    See https://docs.solidjs.com/concepts/components/props#destructuring-props

--- frontend/src/entities/thing/ui/Thing.tsx
+++ frontend/src/entities/thing/ui/Thing.tsx
@@ -8,24 +8,14 @@ import { LostThingProps } from '../model/ThingProps';
 import { FoundThingProps } from '../model/ThingProps';
 
 export const Thing: Component<LostThingProps & FoundThingProps> = (props) => {
-  const custom_text = props.custom_text;
-  const email = props.email;
-  const id = props.id;
-  const page = props.page;
-  const reloadList = props.reloadList;
-  const tabIndex = props.tabIndex;
-  const thing_location = props.thing_location;
-  const thing_name = props.thing_name;
-  const type = props.type;
-
-  const monthNumber = Number(props.publication_date.slice(5, 7));
-  const day = Number(props.publication_date.slice(8, 10));
-  const month = months[monthNumber - 1];
-  const year = props.publication_date.slice(0, 4);
-  const time = props.publication_time;
+  const monthNumber = () => Number(props.publication_date.slice(5, 7));
+  const day = () => Number(props.publication_date.slice(8, 10));
+  const month = () => months[monthNumber() - 1];
+  const year = () => props.publication_date.slice(0, 4);
+  const time = () => props.publication_time;
   const [thingHidden, setThingHidden] = createSignal(false);
 
-  const path_to_photo = `/storage/${type}/${id}.jpeg`;
+  const path_to_photo = `/storage/${props.type}/${props.id}.jpeg`;
 
   // Check if the photo exists and put the result in photoExists()
   const [photoExists, setPhotoExists] = createSignal(false);
@@ -36,27 +26,27 @@ export const Thing: Component<LostThingProps & FoundThingProps> = (props) => {
   return (
     <div class={thingHidden() ? 'thing thing__hidden' : 'thing'}>
       <div class='thing__title'>
-        {thing_name} (№{id})
+        {props.thing_name} (№{props.id})
       </div>
       <div class='thing__content'>
         <div>
           Опубликовано:{' '}
           <b>
-            {day} {month} {year} в {time}
+            {day()} {month()} {year()} в {time()}
           </b>
           <br />
-          {type === 'lost' && (
+          {props.type === 'lost' && (
             <>
-              Email: <b>{email}</b>
+              Email: <b>{props.email}</b>
             </>
           )}
-          {type === 'found' && (
+          {props.type === 'found' && (
             <>
-              Где забрать: <b>{thing_location}</b>
+              Где забрать: <b>{props.thing_location}</b>
             </>
           )}
           <br />
-          <i>{custom_text}</i>
+          <i>{props.custom_text}</i>
         </div>
         {photoExists() && (
           <img
@@ -67,12 +57,14 @@ export const Thing: Component<LostThingProps & FoundThingProps> = (props) => {
         )}
       </div>
       <button
-        tabIndex={tabIndex}
+        tabIndex={props.tabIndex}
         onClick={() => {
           if (confirm('Вы уверены?')) {
             setThingHidden(true);
             setTimeout(() => {
-              changeThingStatus(type, id).then(() => reloadList());
+              changeThingStatus(props.type, props.id).then(() =>
+                props.reloadList(),
+              );
             }, 500);
           }
         }}
