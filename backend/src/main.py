52906553bc2920b4c138c4e166cc89e3a92ed828
laptop-coder <coderlaptop@gmail.com>
2025-06-27 20:53:28 +0300

    refactor(backend): add `rsa_keys.py`

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,73 +1,23 @@
-from PIL import Image
-from argon2 import PasswordHasher
-from cryptography.hazmat.primitives import serialization
-from cryptography.hazmat.primitives.asymmetric.rsa import RSAPrivateKey
-from cryptography.hazmat.primitives.asymmetric.rsa import generate_private_key
-from exceptions import MultipleModeratorsHaveTheSameUsername
-from fastapi import FastAPI, Response
-from fastapi.middleware.cors import CORSMiddleware
 from pathlib import Path
-from pydantic import BaseModel
 from typing import Literal, Optional
 import base64
-import consts
 import datetime
-import jwt
-import os
 import sqlite3
 
+from PIL import Image
+from argon2 import PasswordHasher
+from cryptography.hazmat.primitives.asymmetric.rsa import RSAPrivateKey
+from fastapi import FastAPI, Response
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
+import jwt
 
-# Create RSA keys if not exist
-if not os.path.isfile(consts.PATH_TO_PRIVATE_KEY) and not os.path.isfile(
-    consts.PATH_TO_PUBLIC_KEY
-):
-    private_key = generate_private_key(public_exponent=65537, key_size=4096)
-    public_key = private_key.public_key()
-
-    private_key_serialized = private_key.private_bytes(
-        encoding=serialization.Encoding.PEM,
-        format=serialization.PrivateFormat.PKCS8,
-        encryption_algorithm=serialization.BestAvailableEncryption(
-            consts.PRIVATE_KEY_ENCRYPTION_PASSWORD.encode()
-        ),
-    ).decode()
-    public_key_serialized = public_key.public_bytes(
-        encoding=serialization.Encoding.PEM,
-        format=serialization.PublicFormat.SubjectPublicKeyInfo,
-    ).decode()
-
-    with open(consts.PATH_TO_PRIVATE_KEY, 'w') as file:
-        file.write(private_key_serialized)
-    with open(consts.PATH_TO_PUBLIC_KEY, 'w') as file:
-        file.write(public_key_serialized)
+from exceptions import MultipleModeratorsHaveTheSameUsername
+from auth.rsa_keys import private_key, public_key
+import consts
 
 
-# Read keys
-with open(consts.PATH_TO_PRIVATE_KEY, 'rb') as file:
-    private_key = (
-        serialization.load_pem_private_key(
-            file.read(),
-            password=consts.PRIVATE_KEY_ENCRYPTION_PASSWORD.encode(),
-        )
-        .private_bytes(
-            encoding=serialization.Encoding.PEM,
-            format=serialization.PrivateFormat.PKCS8,
-            encryption_algorithm=serialization.NoEncryption(),
-        )
-        .decode()
-    )
-
-with open(consts.PATH_TO_PUBLIC_KEY, 'rb') as file:
-    public_key = (
-        serialization.load_pem_public_key(file.read())
-        .public_bytes(
-            encoding=serialization.Encoding.PEM,
-            format=serialization.PublicFormat.SubjectPublicKeyInfo,
-        )
-        .decode()
-    )
-
-
+ph = PasswordHasher()
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
@@ -75,9 +25,6 @@ app.add_middleware(
     allow_methods=['*'],
     allow_headers=['*'],
 )
-
-
-ph = PasswordHasher()
 
 
 class LostThingData(BaseModel):
