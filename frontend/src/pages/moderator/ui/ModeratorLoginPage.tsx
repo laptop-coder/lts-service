8b25a7fef8c47bf1684286447c69b7e1ccddf908
laptop-coder <coderlaptop@gmail.com>
2025-07-02 16:06:19 +0300

    feat(frontend): add moderator login form
    Add content to the moderator login page: form, background, etc.

--- frontend/src/pages/moderator/ui/ModeratorLoginPage.tsx
+++ frontend/src/pages/moderator/ui/ModeratorLoginPage.tsx
@@ -2,9 +2,51 @@ import '../../../app/styles.css';
 import type { Component } from 'solid-js';
 
 export const ModeratorLoginPage: Component = () => {
+  document.title = 'Модератор: вход в аккаунт (LTS-сервис)';
   return (
     <div class='page'>
-      <div class='container'></div>
+      <div class='auth-container__wrapper'>
+        <div class='auth-container'>
+          <div class='auth-container__title'>Вход в аккаунт модератора</div>
+          <form class='auth-container__form'>
+            <div class='auth-container__input-group'>
+              <input
+                class='auth-container__input'
+                id='username'
+                // Placeholder is needed here to check if the input field is not empty.
+                // :not(:placeholder-shown)
+                placeholder=''
+              />
+              <label
+                class='auth-container__label'
+                for='username'
+              >
+                Имя пользователя
+              </label>
+              <span class='auth-container__underline' />
+            </div>
+            <div class='auth-container__input-group'>
+              <input
+                class='auth-container__input'
+                id='password'
+                placeholder=''
+                type='password'
+              />
+              <label
+                class='auth-container__label'
+                for='password'
+              >
+                Пароль
+              </label>
+              <span class='auth-container__underline' />
+            </div>
+            <button class='auth-container__submit-button'>Войти</button>
+            <span class='auth-container__another-action'>
+              Нет аккаунта? <a href='/moderator/register'>Регистрация</a>
+            </span>
+          </form>
+        </div>
+      </div>
     </div>
   );
 };
