8c7558069599d815f5527f21dcd33fb3a0606483
laptop-coder <coderlaptop@gmail.com>
2025-07-21 16:13:00 +0300

    feat(backend): add `logger` (based on `slog`)

---
+++ backend/logger.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"io"
+	"log/slog"
+	"os"
+)
+
+func initLogger() *slog.Logger {
+	const PATH_TO_LOG string = "./backend.log" // TODO: move const to .env
+	logfile, err := os.OpenFile(PATH_TO_LOG, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		panic(err)
+	}
+	wrt := io.MultiWriter(os.Stdout, logfile)
+	return slog.New(slog.NewJSONHandler(wrt, nil))
+}
