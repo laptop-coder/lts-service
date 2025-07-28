1369202dee3551aeb12593f16f4cbec8d6e97a4f
laptop-coder <coderlaptop@gmail.com>
2025-07-28 14:51:53 +0300

    build(backend): Docker image

--- backend/main.go
+++ backend/main.go
@@ -4,18 +4,26 @@ import (
 	. "backend/database"
 	"backend/handlers"
 	. "backend/logger"
+	// "flag"
 	"net/http"
 )
 
 func main() {
 	defer DB.Close()
 
+	// var pathToCert string
+	// var pathToKey string
+	// flag.StringVar(&pathToCert, "path-to-cert", "", "Путь к файлу SSL-сертификата")
+	// flag.StringVar(&pathToKey, "path-to-key", "", "Путь к файлу SSL-ключа")
+
 	http.HandleFunc("/thing/change_status", handlers.ChangeThingStatus)
 	http.HandleFunc("/thing/get_data", handlers.GetThingData)
 	http.HandleFunc("/things/get_list", handlers.GetThingsList)
 	http.HandleFunc("/thing/add", handlers.AddThing)
-	Logger.Info("Starting server at port 8080")
-	err := http.ListenAndServe(":8080", nil)
+
+	Logger.Info("Starting server")
+	// err := http.ListenAndServeTLS(":443", pathToCert, pathToKey, nil)
+	err := http.ListenAndServe(":80", nil)
 	if err != nil {
 		Logger.Error("Error starting the server: " + err.Error())
 	}
