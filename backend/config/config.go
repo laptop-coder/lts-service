59c329c5d5af80b2a34e27361ee6371969753629
laptop-coder <coderlaptop@gmail.com>
2025-07-31 13:55:24 +0300

    feat(backend): add config

---
+++ backend/config/config.go
@@ -0,0 +1,92 @@
+package config
+
+import (
+	"fmt"
+	"os"
+)
+
+type AppConfig struct {
+	DevMode string
+}
+
+type DBConfig struct {
+	PathTo string
+}
+
+type EnvConfig struct {
+	PathTo string
+}
+
+type RSAConfig struct {
+	PrivateKeyPassword string
+	PathToPrivateKey   string
+	PathToPublicKey    string
+}
+
+type SSLConfig struct {
+	PathToCert string
+	PathToKey  string
+}
+
+type StorageConfig struct {
+	PathTo string
+}
+
+type Config struct {
+	App     AppConfig
+	DB      DBConfig
+	Env     EnvConfig
+	RSA     RSAConfig
+	SSL     SSLConfig
+	Storage StorageConfig
+}
+
+func New() *Config {
+	return &Config{
+		App: AppConfig{
+			DevMode: getEnv("LTS_SERVICE_DEV_MODE"),
+		},
+		DB: DBConfig{
+			PathTo: getEnv("PATH_TO_DB"),
+		},
+		Env: EnvConfig{
+			PathTo: getEnv("PATH_TO_ENV"),
+		},
+		RSA: RSAConfig{
+			PathToPrivateKey: fmt.Sprintf(
+				"%s/%s",
+				getEnv("PATH_TO_ENV"),
+				getEnv("RSA_PRIVATE_KEY"),
+			),
+			PathToPublicKey: fmt.Sprintf(
+				"%s/%s",
+				getEnv("PATH_TO_ENV"),
+				getEnv("RSA_PUBLIC_KEY"),
+			),
+			PrivateKeyPassword: getEnv("PRIVATE_KEY_ENCRYPTION_PASSWORD"),
+		},
+		SSL: SSLConfig{
+			PathToCert: fmt.Sprintf(
+				"%s/%s",
+				getEnv("PATH_TO_ENV"),
+				getEnv("SSL_CERT"),
+			),
+			PathToKey: fmt.Sprintf(
+				"%s/%s",
+				getEnv("PATH_TO_ENV"),
+				getEnv("SSL_KEY"),
+			),
+		},
+		Storage: StorageConfig{
+			PathTo: getEnv("PATH_TO_STORAGE"),
+		},
+	}
+}
+
+// Read environment variable by the key or panic if it doesn't exist
+func getEnv(key string) string {
+	if value, exists := os.LookupEnv(key); exists {
+		return value
+	}
+	panic(fmt.Sprintf("The required environment variable \"%s\" is not set", key))
+}
