f2622bf9f6cdda06e0c67f6246fce8fe714e2946
laptop-coder <coderlaptop@gmail.com>
2025-08-02 10:13:37 +0300

    style(backend): sort handlers in `main.go`

--- backend/main.go
+++ backend/main.go
@@ -11,10 +11,10 @@ import (
 func main() {
 	defer DB.Close()
 
+	http.HandleFunc("/thing/add", handlers.AddThing)
 	http.HandleFunc("/thing/change_status", handlers.ChangeThingStatus)
 	http.HandleFunc("/thing/get_data", handlers.GetThingData)
 	http.HandleFunc("/things/get_list", handlers.GetThingsList)
-	http.HandleFunc("/thing/add", handlers.AddThing)
 
 	Logger.Info("Starting server")
 	err := http.ListenAndServeTLS(":443", Cfg.SSL.PathToCert, Cfg.SSL.PathToKey, nil)
