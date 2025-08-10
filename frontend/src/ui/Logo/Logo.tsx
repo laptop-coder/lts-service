a6105b913942aedcff2ff8d1b7ef04dcd8b4a75c
laptop-coder <coderlaptop@gmail.com>
2025-08-11 01:12:16 +0300

    feat(frontend): add the logo component

---
+++ frontend/src/ui/Logo/Logo.tsx
@@ -0,0 +1,18 @@
+import { JSX } from "solid-js";
+
+import { A } from "@solidjs/router";
+
+import { HOME_ROUTE } from "../../utils/consts";
+import styles from "./logo.module.css";
+
+const Logo = (): JSX.Element => {
+  return (
+    <A href={HOME_ROUTE}>
+      <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40">
+        <image class={styles.logo} href="/src/assets/logo.svg"></image>
+      </svg>
+    </A>
+  );
+};
+
+export default Logo;
