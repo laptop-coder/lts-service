2386a81a0015afac340ae9656deb7101995bc91b
laptop-coder <coderlaptop@gmail.com>
2025-08-11 01:13:01 +0300

    refactor(frontend): use `HOME_ROUTE` constant

--- frontend/src/AppRouter.tsx
+++ frontend/src/AppRouter.tsx
@@ -3,12 +3,13 @@ import { JSX } from "solid-js";
 import { Router, Route, Navigate } from "@solidjs/router";
 
 import HomePage from "./pages/HomePage";
+import { HOME_ROUTE } from "./utils/consts";
 
 const AppRouter = (): JSX.Element => {
   return (
     <Router>
-      <Route path="/" component={() => <HomePage />} />
-      <Route path="/*" component={() => <Navigate href="/" state />} />
+      <Route path={HOME_ROUTE} component={() => <HomePage />} />
+      <Route path="/*" component={() => <Navigate href={HOME_ROUTE} state />} />
     </Router>
   );
 };
