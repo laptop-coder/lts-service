d8b8ef1a5984028b6e4ace55d3a33adf92a04b92
laptop-coder <coderlaptop@gmail.com>
2025-08-11 01:44:20 +0300

    refactor(frontend): import CSS from `index.tsx`

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -3,6 +3,8 @@ import { render } from 'solid-js/web';
 
 import AppRouter from './AppRouter';
 
+import './css/fonts.css';
+
 const root = document.getElementById('root');
 
 render(() => <AppRouter />, root!);
