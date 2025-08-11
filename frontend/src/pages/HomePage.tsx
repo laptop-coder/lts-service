934f82fef7ec55ba4425f10f139d02cb97bd10ee
laptop-coder <coderlaptop@gmail.com>
2025-08-11 22:49:21 +0300

    feat(frontend): add components to the home page

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,7 +1,26 @@
-import { JSX } from "solid-js";
+import { JSX } from 'solid-js';
+
+import Header from '../components/Header/Header';
+import Content from '../components/Content/Content';
+import Footer from '../components/Footer/Footer';
+import Page from '../ui/Page/Page';
+import SquareImageButton from '../ui/SquareImageButton/SquareImageButton';
 
 const HomePage = (): JSX.Element => {
-  return <div></div>;
+  return (
+    <Page>
+      <Header>
+        <SquareImageButton>
+          <img src='/src/assets/add.svg'></img>
+        </SquareImageButton>
+        <SquareImageButton>
+          <img src='/src/assets/reload.svg'></img>
+        </SquareImageButton>
+      </Header>
+      <Content></Content>
+      <Footer></Footer>
+    </Page>
+  );
 };
 
 export default HomePage;
