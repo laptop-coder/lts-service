0bd8a7477929f1624e2cf6af9d200c463c38a940
laptop-coder <coderlaptop@gmail.com>
2025-08-11 21:07:01 +0300

    refactor(frontend): header, logo

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -9,10 +9,13 @@ import { HOME_ROUTE } from '../../utils/consts';
 const Header = (props: ParentProps): JSX.Element => {
   return (
     <header class={styles.header}>
-      <Logo />
-      <A href={HOME_ROUTE}>
-        <h1>Сервис поиска потерянных вещей</h1>
-      </A>
+      {/* Main module is the left part of the header*/}
+      <div class={styles.main_module}>
+        <Logo />
+        <A href={HOME_ROUTE}>
+          <h1>Сервис поиска потерянных вещей</h1>
+        </A>
+      </div>
       {/* Custom module is the right part of the header*/}
       <div class={styles.custom_module}>{props.children}</div>
     </header>
