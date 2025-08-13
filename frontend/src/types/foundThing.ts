86dd402337db7c5bda56b95cfb806feea26f3240
laptop-coder <coderlaptop@gmail.com>
2025-08-13 11:49:11 +0300

    chore(frontend): add things types

---
+++ frontend/src/types/foundThing.ts
@@ -0,0 +1,11 @@
+interface foundThing {
+  FoundThingId: number;
+  PublicationDatetime: string;
+  ThingName: string;
+  ThingLocation: string;
+  CustomText: string;
+  Verified: number;
+  Status: number;
+}
+
+export default foundThing;
---
+++ frontend/src/types/lostThing.ts
@@ -0,0 +1,11 @@
+interface lostThing {
+  LostThingId: number;
+  PublicationDatetime: string;
+  ThingName: string;
+  UserEmail: string;
+  CustomText: string;
+  Verified: number;
+  Status: number;
+}
+
+export default lostThing;
