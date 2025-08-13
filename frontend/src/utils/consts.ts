c97b0c966f17f94d2c68e9356907c41ccc10f76a
laptop-coder <coderlaptop@gmail.com>
2025-08-13 11:27:11 +0300

    chore(frontend): add list of months

--- frontend/src/utils/consts.ts
+++ frontend/src/utils/consts.ts
@@ -2,3 +2,18 @@ export const HOME_ROUTE = '/';
 
 export const BACKEND_URL = 'https://172.16.1.2';
 export const SOURCE_CODE_URL = 'https://github.com/laptop-coder/lts-service';
+
+export const months = [
+  'января',
+  'февраля',
+  'марта',
+  'апреля',
+  'мая',
+  'июня',
+  'июля',
+  'августа',
+  'сентября',
+  'октября',
+  'ноября',
+  'декабря',
+];
