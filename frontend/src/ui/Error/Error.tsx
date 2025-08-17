d5f47d09ef9b96b9f2cbb2179f433a0362360d13
laptop-coder <coderlaptop@gmail.com>
2025-08-17 23:46:36 +0300

    feat(frontend): add error message

--- frontend/src/ui/Error/Error.tsx
+++ frontend/src/ui/Error/Error.tsx
@@ -1,11 +1,12 @@
-import { JSX } from 'solid-js';
+import { JSX, ParentProps } from 'solid-js';
 
 import styles from './Error.module.css';
 
-const Error = (): JSX.Element => (
+const Error = (props: ParentProps): JSX.Element => (
   <div class={styles.error}>
     <img src='/src/assets/error.svg' />
     Ошибка!
+    {props.children && ' ' + props.children}
   </div>
 );
 
