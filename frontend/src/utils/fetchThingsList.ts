cfe2e7153a762318e5426e7057ca4829c9a30d7e
laptop-coder <coderlaptop@gmail.com>
2025-08-17 23:32:46 +0300

    refactor(frontend): catch errors

--- frontend/src/utils/fetchThingsList.ts
+++ frontend/src/utils/fetchThingsList.ts
@@ -5,7 +5,8 @@ const fetchThingsList = async (thingsType: 'lost' | 'found') => {
     .get(`/things/get_list?things_type=${thingsType}`)
     .then((response) => {
       return response.data;
-    });
+    })
+    .catch((error) => console.log(error));
 };
 
 export default fetchThingsList;
