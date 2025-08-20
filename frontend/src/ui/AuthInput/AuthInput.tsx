fa6aa4f1105317bdd80b737f6994c04a88f8d0fc
laptop-coder <coderlaptop@gmail.com>
2025-08-20 11:54:39 +0300

    feat(frontend): add auth input (JSX element)

---
+++ frontend/src/ui/AuthInput/AuthInput.tsx
@@ -0,0 +1,29 @@
+import { JSX } from 'solid-js';
+
+import styles from './AuthInput.module.css';
+
+const AuthInput = (
+  props: JSX.InputHTMLAttributes<HTMLInputElement>,
+): JSX.Element => (
+  <div class={styles.auth_input_group}>
+    <input
+      class={styles.auth_input}
+      type={props.type || 'text'}
+      // Placeholder is needed here to check if the input field is not empty.
+      // Using :not(:placeholder-shown)
+      placeholder=''
+      value={props.value}
+      onchange={props.onchange}
+      id={props.id}
+    />
+    <label
+      class={styles.auth_input_label}
+      for={props.id}
+    >
+      {props.placeholder}
+    </label>
+    <span class={styles.auth_input_underline} />
+  </div>
+);
+
+export default AuthInput;
