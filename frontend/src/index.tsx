46a6ce32a504b933e6fcd333c5d9f429b2d6d2cc
laptop-coder <coderlaptop@gmail.com>
2025-11-29 16:30:52 +0300

    feat(frontend): add `index.css` and `index.tsx`

---
+++ frontend/src/index.tsx
@@ -0,0 +1,11 @@
+/* @refresh reload */
+import { render } from 'solid-js/web';
+
+import AppRouter from './AppRouter';
+
+import './css/fonts.css';
+import './index.css';
+
+const root = document.getElementById('root');
+
+render(() => <AppRouter />, root!);
