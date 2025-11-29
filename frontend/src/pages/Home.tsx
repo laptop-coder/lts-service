cf8b6a30df7237f11d8fef7a98faecb72db65d6a
laptop-coder <coderlaptop@gmail.com>
2025-11-29 16:39:02 +0300

    feat(frontend): add pages

---
+++ frontend/src/pages/Home.tsx
@@ -0,0 +1,39 @@
+import { JSX, createSignal } from 'solid-js';
+
+import Page from '../ui/Page/Page';
+import Header from '../ui/Header/Header';
+import Content from '../ui/Content/Content';
+import Footer from '../ui/Footer/Footer';
+import { Role, ThingType } from '../utils/consts';
+import getAuthorizedCookie from '../utils/getAuthorizedCookie';
+import ThingsTypeToggle from '../components/ThingsTypeToggle';
+import ThingsList from '../components/ThingsList/ThingsList';
+
+const HomePage = (): JSX.Element => {
+  const [authorized, setAuthorized] = createSignal(false);
+  getAuthorizedCookie(setAuthorized);
+
+  const [thingsType, setThingsType] = createSignal(ThingType.lost);
+
+  return (
+    <Page
+      role={authorized() ? Role.user : Role.none}
+      authorized={authorized()}
+    >
+      <Header
+        role={authorized() ? Role.user : Role.none}
+        authorized={authorized()}
+      />
+      <Content>
+        <ThingsTypeToggle setter={setThingsType} />
+        <ThingsList
+          thingsType={thingsType}
+          role={Role.user}
+        />
+      </Content>
+      <Footer />
+    </Page>
+  );
+};
+
+export default HomePage;
---
+++ frontend/src/pages/LoginModerator.tsx
@@ -0,0 +1,28 @@
+import { JSX, createSignal } from 'solid-js';
+
+import Page from '../ui/Page/Page';
+import Header from '../ui/Header/Header';
+import Content from '../ui/Content/Content';
+import Footer from '../ui/Footer/Footer';
+import { Role } from '../utils/consts';
+import getAuthorizedCookie from '../utils/getAuthorizedCookie';
+
+const LoginModeratorPage = (): JSX.Element => {
+  const [authorized, setAuthorized] = createSignal(false);
+  getAuthorizedCookie(setAuthorized);
+  return (
+    <Page
+      role={Role.none}
+      authorized={authorized()}
+    >
+      <Header
+        role={Role.moderator}
+        authorized={authorized()}
+      />
+      <Content></Content>
+      <Footer />
+    </Page>
+  );
+};
+
+export default LoginModeratorPage;
