9a34a6ace14701751466c90e709467cefea6fe17
laptop-coder <coderlaptop@gmail.com>
2026-01-06 22:20:05 +0300

    feat(backend): add config

---
+++ backend/internal/config/config.go
@@ -0,0 +1,66 @@
+// Package config provides constants, settings and environment variables to use
+// in code.
+package config
+
+import (
+	"backend/internal/types"
+	"fmt"
+	"os"
+	"path/filepath"
+)
+
+func newConfig() *types.Config {
+	return &types.Config{
+		App: types.AppConfig{
+			PortBackend:  "37190",
+			PortFrontend: GetEnv("FRONTEND_PORT"),
+		},
+		Bcrypt: types.BcryptConfig{
+			Cost: 15, // minimal is 4, maximum is 31, default is 10
+		},
+		Postgres: types.PostgresConfig{
+			DBName:   GetEnv("POSTGRES_DB"),
+			Password: GetEnv("POSTGRES_PASSWORD"),
+			Port:     GetEnv("POSTGRES_PORT"),
+			SSLMode:  GetEnv("POSTGRES_SSL_MODE"),
+			User:     GetEnv("POSTGRES_USER"),
+		},
+		Env: types.EnvConfig{
+			PathTo: GetEnv("PATH_TO_ENV"),
+		},
+		Logs: types.LogsConfig{
+			PathToBackend: filepath.Join(
+				GetEnv("PATH_TO_LOGS"),
+				GetEnv("BACKEND_LOG"),
+			),
+		},
+		RSA: types.RSAConfig{
+			PathToPrivateKey: filepath.Join(
+				GetEnv("PATH_TO_ENV"),
+				GetEnv("RSA_PRIVATE_KEY"),
+			),
+			PathToPublicKey: filepath.Join(
+				GetEnv("PATH_TO_ENV"),
+				GetEnv("RSA_PUBLIC_KEY"),
+			),
+		},
+		Role: types.RolesConfig{
+			User:      "user",
+			Moderator: "moderator",
+		},
+		Storage: types.StorageConfig{
+			PathTo: GetEnv("PATH_TO_STORAGE"),
+		},
+	}
+}
+
+// GetEnv allows to read environment variable by the key. If it doesn't exist,
+// the function will cause panic.
+func GetEnv(key string) string {
+	if value, exists := os.LookupEnv(key); exists {
+		return value
+	}
+	panic(fmt.Sprintf("The required environment variable \"%s\" is not set", key))
+}
+
+var Cfg = newConfig()
