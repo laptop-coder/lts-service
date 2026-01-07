d22db1ea4791f4bb9ed3c6cda0dc2f6dc46ba2f1
laptop-coder <coderlaptop@gmail.com>
2026-01-07 12:17:43 +0300

    feat(backend): add aliases to the logger

--- backend/pkg/logger/logger.go
+++ backend/pkg/logger/logger.go
@@ -1,4 +1,4 @@
-// Package logger provides slog instance for writing logs to stdout
+// Package logger provides aliases for writing logs to stdout
 package logger
 
 import (
@@ -7,13 +7,37 @@ import (
 	"os"
 )
 
-var Log = slog.New(
+// slog instance
+var log = slog.New(
 	slog.NewJSONHandler(
 		io.Writer(
 			os.Stdout,
 		),
 		&slog.HandlerOptions{
-			Level: slog.LevelInfo,
+			Level: slog.LevelDebug,
 		},
 	),
 )
+
+
+// Aliases
+
+// Debug is alias for writing logs to stdout at the debug (-4) level
+func Debug (message string) {
+	log.Debug(message)
+}
+
+// Info is alias for writing logs to stdout at the info (0) level
+func Info (message string) {
+	log.Info(message)
+}
+
+// Warn is alias for writing logs to stdout at the warning (4) level
+func Warn (message string) {
+	log.Warn(message)
+}
+
+// Error is alias for writing logs to stdout at the error (8) level
+func Error (message string) {
+	log.Error(message)
+}
