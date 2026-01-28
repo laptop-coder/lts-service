a13bfe04be90846f1462ac13de1889aa56d193e2
laptop-coder <coderlaptop@gmail.com>
2026-01-28 12:40:13 +0300

    feat(backend): add database package

---
+++ backend/internal/database/db.go
@@ -0,0 +1,73 @@
+package database
+
+import (
+	"fmt"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+)
+
+type Config struct {
+	DBName   string
+	Host     string
+	Password string
+	Port     int
+	SSLMode  string // TODO: rewrite to bool
+	TimeZone string
+	User     string
+}
+
+func Connect(config Config) (*gorm.DB, error) {
+	dsn := fmt.Sprintf(
+		"host=%s user=%s password=%s dbname=%s port=%d sslmode=%s TimeZone=%s",
+		config.Host,
+		config.User,
+		config.Password,
+		config.DBName,
+		config.Port, // TODO: move here logic to convert true/false to "enable"/"disable"
+		config.SSLMode,
+		config.TimeZone,
+	)
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{
+		Logger: logger.Default.LogMode(logger.Info),
+	})
+	if err != nil {
+		return nil, fmt.Errorf("failed to connect to database: %w", err)
+	}
+	return db, nil
+}
+
+func Close(db *gorm.DB) error {
+	if db == nil {
+		return nil
+	}
+	sqlDB, err := db.DB()
+	if err != nil {
+		return fmt.Errorf("failed to get database instance: %w", err)
+	}
+	return sqlDB.Close()
+}
+
+func Migrate(db *gorm.DB, models []any) error {
+	if err := db.AutoMigrate(models...); err != nil {
+		return fmt.Errorf("failed to migrate database: %w", err)
+	}
+	return nil
+}
+
+func HeathCheck(db *gorm.DB) error {
+	if db == nil {
+		return fmt.Errorf("database is nil")
+	}
+
+	sqlDB, err := db.DB()
+	if err != nil {
+		return fmt.Errorf("failed to get database instance: %w", err)
+	}
+
+	return sqlDB.Ping()
+}
+
+func Transaction(db *gorm.DB, fn func(tx *gorm.DB) error) error {
+	return db.Transaction(fn)
+}
