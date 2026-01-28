eff6c671b991249c84c98fc86d504fbd673a74d8
laptop-coder <coderlaptop@gmail.com>
2026-01-28 12:32:46 +0300

    feat(backend): add logger

--- backend/pkg/logger/logger.go
+++ backend/pkg/logger/logger.go
@@ -1,43 +1,29 @@
-// Package logger provides aliases for writing logs to stdout
 package logger
 
 import (
-	"io"
 	"log/slog"
 	"os"
 )
 
-// slog instance
-var log = slog.New(
-	slog.NewJSONHandler(
-		io.Writer(
-			os.Stdout,
-		),
+type Logger interface {
+	Debug(msg string, args ...any)
+	Info(msg string, args ...any)
+	Warn(msg string, args ...any)
+	Error(msg string, args ...any)
+}
+
+type logger struct {
+	*slog.Logger
+}
+
+func New() Logger {
+	handler := slog.NewJSONHandler(
+		os.Stdout,
 		&slog.HandlerOptions{
 			Level: slog.LevelDebug,
 		},
-	),
-)
-
-
-// Aliases
-
-// Debug is alias for writing logs to stdout at the debug (-4) level
-func Debug (message string) {
-	log.Debug(message)
-}
-
-// Info is alias for writing logs to stdout at the info (0) level
-func Info (message string) {
-	log.Info(message)
-}
-
-// Warn is alias for writing logs to stdout at the warning (4) level
-func Warn (message string) {
-	log.Warn(message)
-}
-
-// Error is alias for writing logs to stdout at the error (8) level
-func Error (message string) {
-	log.Error(message)
+	)
+	return &logger{
+		Logger: slog.New(handler),
+	}
 }
