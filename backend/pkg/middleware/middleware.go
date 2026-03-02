0c51bd9802058c98608f73ac148400ce3e6b77fc
laptop-coder <coderlaptop@gmail.com>
2026-03-02 14:33:37 +0300

    feat(backend): add middleware for JWT
    Get JWT access from cookie, validate it, parse, and then put user ID
    and roles to context.

--- backend/pkg/middleware/middleware.go
+++ backend/pkg/middleware/middleware.go
@@ -1,7 +1,10 @@
 package middleware
 
 import (
+	"backend/internal/service"
+	"backend/pkg/helpers"
 	"backend/pkg/logger"
+	"context"
 	"net/http"
 	"time"
 )
@@ -16,4 +19,33 @@ func Logging(log logger.Logger, next http.Handler) http.Handler {
 			"duration", time.Since(start),
 		)
 	})
+}
+
+type contextKey string
+
+const UserIDKey contextKey = "user_id"
+const UserRolesKey contextKey = "user_roles"
+
+func Auth(authService service.AuthService) func(http.Handler) http.Handler {
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			// Get JWT access from cookies
+			cookie, err := helpers.GetCookie("jwt_access", r)
+			if err != nil {
+				helpers.ErrorResponse(w, "unauthorized", http.StatusUnauthorized) // TODO: maybe improve error handling
+				return
+			}
+			// Validate token
+			claims, err := authService.ParseToken(cookie)
+			if err != nil {
+				helpers.ErrorResponse(w, "invalid token", http.StatusUnauthorized) // TODO: maybe improve error handling
+				return
+			}
+			// Put user ID and roles to the context
+			ctx := context.WithValue(r.Context(), UserIDKey, claims.UserID)
+			ctx = context.WithValue(ctx, UserRolesKey, claims.Roles)
+			//
+			next.ServeHTTP(w, r.WithContext(ctx))
+		})
+	}
 }
