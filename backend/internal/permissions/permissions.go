80afc8432856a339a7a98f00f62dcae18af3feea
laptop-coder <coderlaptop@gmail.com>
2026-03-08 01:23:54 +0300

    feat(backend): update list of permissions

--- backend/internal/permissions/permissions.go
+++ backend/internal/permissions/permissions.go
@@ -3,104 +3,105 @@ package permissions
 
 // Post permissions
 const (
-	// ability to create new posts
-	PostCreate = "posts.create"
-	// ability to view posts
-	PostRead = "posts.read"
-	// ability to update any post
-	PostUpdate = "posts.update"
-	// ability to update own posts
-	PostUpdateOwn = "posts.update.own"
-	// ability to delete any post
-	PostDelete = "posts.delete"
-	// ability to delete own posts
-	PostDeleteOwn = "posts.delete.own"
-	// ability to verify posts (moderator action)
-	PostVerify = "posts.verify"
+	PostCreate          = "post.create"
+	PostReadAny         = "post.read.any"
+	PostReadVerified    = "post.read.verified"
+	PostReadOwn         = "post.read.own"
+	PostUpdateAny       = "post.update.any"
+	PostUpdateOwn       = "post.update.own"
+	PostDeleteAny       = "post.delete.any"
+	PostDeleteOwn       = "post.delete.own"
+	PostVerify          = "post.verify"
+	PostMarkReturnedAny = "post.mark.returned.any"
+	PostMarkReturnedOwn = "post.mark.returned.own"
 )
 
 // User permissions
 const (
-	// ability to create new users
-	UserCreate = "users.create"
-	// ability to view users
-	UserRead = "users.read"
-	// ability to update any user
-	UserUpdate = "users.update"
-	// ability to update own profile
-	UserUpdateOwn = "users.update.own"
-	// ability to delete any user
-	UserDelete = "users.delete"
-	// ability to delete own account
-	UserDeleteOwn = "users.delete.own"
+	UserCreate           = "user.create"
+	UserReadOwn          = "user.read.own"
+	UserReadOther        = "user.read.other"
+	UserReadAll          = "user.read.all"
+	UserUpdateOwn        = "user.update.own"
+	UserDeleteAny        = "user.delete.any"
+	UserDeleteOwn        = "user.delete.own"
+	UserReadOwnSubjects  = "user.read.own.subjects"
+	TeacherReadClassroom = "teacher.read.classroom"
+	StudentReadClassroom = "student.read.classroom"
+	TeacherStudentsRead  = "teacher.students.read"
+	ParentStudentsRead   = "parent.students.read"
+	StudentTeacherRead   = "student.teacher.read"
+	StudentParentsRead   = "student.parents.read"
 )
 
 // Room permissions
 const (
-	// ability to create rooms
-	RoomCreate = "rooms.create"
-	// ability to view rooms
-	RoomRead = "rooms.read"
-	// ability to update rooms
-	RoomUpdate = "rooms.update"
-	// ability to delete rooms
-	RoomDelete = "rooms.delete"
+	RoomCreate  = "room.create"
+	RoomReadAny = "room.read.any"
+	RoomUpdate  = "room.update"
+	RoomDelete  = "room.delete"
 )
 
 // Subject permissions
 const (
-	// ability to create subjects
-	SubjectCreate = "subjects.create"
-	// ability to view subjects
-	SubjectRead = "subjects.read"
-	// ability to update subjects
-	SubjectUpdate = "subjects.update"
-	// ability to delete subjects
-	SubjectDelete = "subjects.delete"
+	SubjectCreate    = "subject.create"
+	SubjectReadAny   = "subject.read.any"
+	SubjectUpdateAny = "subject.update.any"
+	SubjectDeleteAny = "subject.delete.any"
 )
 
-// StudentGroup permissions
+// Student group permissions
 const (
-	// ability to create student groups
-	GroupCreate = "groups.create"
-	// ability to view student groups
-	GroupRead = "groups.read"
-	// ability to update student groups
-	GroupUpdate = "groups.update"
-	// ability to delete student groups
-	GroupDelete = "groups.delete"
-	// ability to assign advisor to student group
-	GroupAssignAdvisor = "groups.assign.advisor"
+	StudentGroupCreate          = "student.group.create"
+	StudentGroupReadAny         = "student.group.read.any"
+	StudentGroupReadOwn         = "student.group.read.own"
+	StudentGroupUpdate          = "student.group.update"
+	StudentGroupDelete          = "student.group.delete"
+	StudentGroupAdvisorAssign   = "student.group.advisor.assign"
+	StudentGroupAdvisorUnassign = "student.group.advisor.unassign"
+	StudentGroupAdvisorRead     = "student.group.advisor.read"
 )
 
 // Teacher permissions
 const (
-	// ability to assign subjects to teacher
-	TeacherAssignSubject = "teacher.assign.subject"
-	// ability to assign classroom to teacher
-	TeacherAssignClassroom = "teacher.assign.classroom"
-)
-
-// Student permissions
-const (
-	// ability to assign parent to student
-	StudentAssignParent = "student.assign.parent"
+	TeacherSubjectReadAny       = "teacher.subject.read.any"
+	TeacherSubjectReadOwn       = "teacher.subject.read.own"
+	TeacherSubjectAssignAny     = "teacher.subject.assign.any"
+	TeacherSubjectAssignOwn     = "teacher.subject.assign.own"
+	TeacherSubjectUnassignAny   = "teacher.subject.unassign.any"
+	TeacherSubjectUnassignOwn   = "teacher.subject.unassign.own"
+	TeacherClassroomReadAny     = "teacher.classroom.read.any"
+	TeacherClassroomReadOwn     = "teacher.classroom.read.own"
+	TeacherClassroomAssignAny   = "teacher.classroom.assign.any"
+	TeacherClassroomAssignOwn   = "teacher.classroom.assign.own"
+	TeacherClassroomUnassignAny = "teacher.classroom.unassign.any"
+	TeacherClassroomUnassignOwn = "teacher.classroom.unassign.own"
 )
 
 // Parent permissions
 const (
-	// ability to view parent's students
-	ParentViewStudents = "parent.view.students"
+	ParentStudentReadAny     = "parent.student.read.any"
+	ParentStudentReadOwn     = "parent.student.read.own"
+	ParentStudentAssignAny   = "parent.student.assign.any"
+	ParentStudentAssignOwn   = "parent.student.assign.own"
+	ParentStudentUnassignAny = "parent.student.unassign.any"
+	ParentStudentUnassignOwn = "parent.student.unassign.own"
 )
 
-// Role permissions
+// User roles permissions
 const (
-	// ability to assign roles to users
-	RoleAssign = "roles.assign"
-	// ability to create new roles
-	RoleCreate = "roles.create"
-	// ability to update roles
-	RoleUpdate = "roles.update"
-	// ability to delete roles
-	RoleDelete = "roles.delete"
+	RoleAssign  = "role.assign"
+	RoleAdd     = "role.add"
+	RoleDelete  = "role.delete"
+	RoleReadAny = "role.read.any"
+	RoleReadOwn = "role.read.own"
+)
+
+// Permissions to work with tokens
+const (
+	TokenInviteAdminCreate = "token.invite.admin.create"
+	TokenInviteUserCreate  = "token.invite.user.create"
+	TokenInviteAdminDelete = "token.invite.admin.delete"
+	TokenInviteUserDelete  = "token.invite.user.delete"
+	TokenInviteRead        = "token.invite.read"
 )
