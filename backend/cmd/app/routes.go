41eb728c2cccccaaed109b20243fd5284b86324c
laptop-coder <coderlaptop@gmail.com>
2026-03-21 19:20:53 +0300

    feat(backend):add route to assign advisor to group

--- backend/cmd/app/routes.go
+++ backend/cmd/app/routes.go
@@ -54,6 +54,7 @@ func SetupRoutes(
 	mux.Handle("GET /api/v1/student_groups/{id}", authMiddleware(requirePermissions(false, permissions.StudentGroupReadAny)(http.HandlerFunc(studentGroupHandler.GetStudentGroupByID))))
 	mux.Handle("GET /api/v1/student_groups/{id}/advisor", authMiddleware(requirePermissions(false, permissions.StudentGroupAdvisorRead)(http.HandlerFunc(studentGroupHandler.GetAdvisorByGroupID))))
 	mux.Handle("DELETE /api/v1/student_groups/{id}", authMiddleware(requirePermissions(false, permissions.StudentGroupDelete)(http.HandlerFunc(studentGroupHandler.Delete))))
+	mux.Handle("POST /api/v1/student_groups/{id}/advisor", authMiddleware(requirePermissions(false, permissions.StudentGroupAdvisorAssign)(http.HandlerFunc(studentGroupHandler.AssignAdvisor))))
 	// Auth
 	mux.Handle("DELETE /api/v1/users/{id}", authMiddleware(requirePermissions(false, permissions.UserDeleteAny)(http.HandlerFunc(authHandler.DeleteAccount))))
 	mux.Handle("DELETE /api/v1/users/me", authMiddleware(requirePermissions(false, permissions.UserDeleteOwn)(http.HandlerFunc(authHandler.DeleteOwnAccount))))
--- backend/internal/handler/student_group_handler.go
+++ backend/internal/handler/student_group_handler.go
@@ -160,3 +160,54 @@ func (h *StudentGroupHandler) Delete(w http.ResponseWriter, r *http.Request) {
 	// Return response
 	helpers.JsonResponse(w, map[string]interface{}{}, http.StatusNoContent)
 }
+
+func (h *StudentGroupHandler) AssignAdvisor(w http.ResponseWriter, r *http.Request) {
+	// Check method
+	if r.Method != http.MethodPost {
+		helpers.ErrorResponse(w, "method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+	// Get and convert (to uint16) student group ID
+	studentGroupID64, err := strconv.ParseUint(r.PathValue("id"), 10, 16)
+	if err != nil {
+		helpers.ErrorResponse(w, "cannot convert student group ID from string to uint64", http.StatusBadRequest) // TODO: use BadRequest instead of InternalServerError in the whole code like here (when cannot convert parameter)
+		return
+	}
+	studentGroupID := uint16(studentGroupID64)
+	// Restrictions
+	r.Body = http.MaxBytesReader(w, r.Body, 1<<20) // 1 MB
+	// Parse form
+	if err := r.ParseForm(); err != nil {
+		helpers.ErrorResponse(w, "failed to parse x-www-form-urlencoded form", http.StatusBadRequest)
+		return
+	}
+	// Get and convert user ID
+	userIDFields := r.PostForm["userId"]
+	if len(userIDFields) != 1 {
+		helpers.ErrorResponse(w, "failed to parse form: userID value must be provided exactly once", http.StatusBadRequest)
+		return
+	}
+	userID, err := uuid.Parse(userIDFields[0])
+	if err != nil {
+		helpers.ErrorResponse(w, "cannot convert user id to uuid", http.StatusBadRequest)
+		return
+	}
+	// Assign advisor
+	if err := h.studentGroupService.AssignAdvisor(r.Context(), studentGroupID, userID); err != nil {
+		helpers.HandleServiceError(w, err)
+		return
+	}
+	// Get updated student group
+	studentGroup, err := h.studentGroupService.GetStudentGroupByID(r.Context(), studentGroupID)
+	if err != nil {
+		helpers.HandleServiceError(w, err)
+		return
+	}
+	// Return response
+	helpers.JsonResponse(w, map[string]interface{}{
+		"studentGroup": studentGroup,
+		"message":      "advisor assigned successfully",
+	}, http.StatusCreated) // TODO: check that in other, e.g., POST requests
+	                       // there is 201 instead of 200. The same for other
+						   // requests.
+}
