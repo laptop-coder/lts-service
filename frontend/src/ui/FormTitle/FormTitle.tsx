fe91653b5d18214d28c93fd68b00b7936f8aae31
laptop-coder <coderlaptop@gmail.com>
2026-03-28 11:54:51 +0300

    feat(frontend): remove old code
    Start frontend rewriting.

--- frontend/src/ui/FormTitle/FormTitle.tsx
+++
@@ -1,9 +0,0 @@
-import { JSX, ParentProps } from 'solid-js';
-
-import styles from './FormTitle.module.css';
-
-const FormTitle = (props: ParentProps): JSX.Element => (
-  <h2 class={styles.form_title}>{props.children}</h2>
-);
-
-export default FormTitle;
--- frontend/src/ui/NoData/NoData.tsx
+++
@@ -1,7 +0,0 @@
-import { JSX } from 'solid-js';
-
-import styles from './NoData.module.css';
-
-const NoData = (): JSX.Element => <div class={styles.no_data}>Данных нет</div>;
-
-export default NoData;
