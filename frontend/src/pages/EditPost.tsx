184964e1d2fbdc86c9185351ffbc2c797e4f294e
laptop-coder <coderlaptop@gmail.com>
2026-04-26 22:42:06 +0300

    fix(frontend): correct height of the buttons

--- frontend/src/pages/EditPost.tsx
+++ frontend/src/pages/EditPost.tsx
@@ -207,14 +207,14 @@ const EditPost = () => {
                 <button
                   type="button"
                   onClick={() => navigate("/")}
-                  class="flex-1 px-4 h-40 bg-gray-100 text-gray-700 rounded-xl hover:bg-gray-200 transition font-medium cursor-pointer"
+                  class="flex-1 px-4 h-10 bg-gray-100 text-gray-700 rounded-xl hover:bg-gray-200 transition font-medium cursor-pointer"
                 >
                   Отмена
                 </button>
                 <button
                   type="submit"
                   disabled={loading()}
-                  class="flex-1 px-4 h-40 bg-blue-600 text-white rounded-xl hover:bg-blue-700 transition font-medium disabled:opacity-50 disabled:cursor-not-allowed cursor-pointer"
+                  class="flex-1 px-4 h-10 bg-blue-600 text-white rounded-xl hover:bg-blue-700 transition font-medium disabled:opacity-50 disabled:cursor-not-allowed cursor-pointer"
                 >
                   Сохранить
                 </button>
