b921280b645c0d4d3ae3cf8b098107f353566c95
laptop-coder <coderlaptop@gmail.com>
2026-08-06 00:22:28 +0300

    refactor: remove unused backup script

--- scripts/make_backup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-BASE_DIR="$HOME/lost-things-search"
-DATA_DIR="data"
-PATH_TO_BACKUPS="$HOME/backups"
-tar -C "$BASE_DIR" -cf "$PATH_TO_BACKUPS/backup_lts_$(date '+%Y-%m-%d_%H-%M-%S').tar" "$DATA_DIR"
-
